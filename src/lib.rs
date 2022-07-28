689795a5d45c06d8bfb874cabd7fe60ca6e42573
poffo <ltkpoffo@gmail.com>
2022-07-28 02:19:59 -0300

    Add usage test

--- src/lib.rs
+++ src/lib.rs
@@ -61,7 +61,7 @@ impl BitWriter {
         self.write_bits(value as u32, 1)
     }
 
-    pub fn write_byte(&mut self, byte: u8) -> Result<(), io::Error> {
+    pub fn write_u8(&mut self, byte: u8) -> Result<(), io::Error> {
         self.write_bits(byte as u32, 8)
     }
 
@@ -116,7 +116,7 @@ impl BitWriter {
                 t |= 0b10000000u8;
             }
 
-            self.write_byte(t)?;
+            self.write_u8(t)?;
 
             if !more_to_write {
                 return Ok(());
@@ -125,7 +125,7 @@ impl BitWriter {
 
         // If we got here, it means we have bits at the highest byte
         // We use the full 8 bits at the last step
-        self.write_byte(value as u8)
+        self.write_u8(value as u8)
     }
 
     pub fn write_varint_i16(&mut self, value: i16) -> Result<(), io::Error> {
@@ -269,7 +269,7 @@ impl<'a> BitReader<'a> {
         Ok(self.read_bits(1)? == 1)
     }
 
-    pub fn read_byte(&mut self) -> Result<u8, io::Error> {
+    pub fn read_u8(&mut self) -> Result<u8, io::Error> {
         Ok(self.read_bits(8)? as u8)
     }
 
@@ -314,7 +314,7 @@ impl<'a> BitReader<'a> {
     pub fn read_varint_u64(&mut self) -> Result<u64, io::Error> {
         let mut result: u64 = 0;
         for i in 0..8 {
-            let byte = self.read_byte()?;
+            let byte = self.read_u8()?;
             // Retrieve the flag stored in the last bit
             let stop_reading = (byte & 0b10000000u8) == 0;
 
@@ -329,7 +329,7 @@ impl<'a> BitReader<'a> {
 
         // If we got here, it means we have bits at the highest byte
         // We use the full 8 bits at the last step
-        let value = self.read_byte()? as u64;
+        let value = self.read_u8()? as u64;
         result |= value << 56;
 
         Ok(result)
@@ -436,6 +436,96 @@ mod tests {
     use super::*;
     use bincode;
     use serde::{Deserialize, Serialize};
+
+    #[test]
+    fn usage() {
+        let mut writer = BitWriter::default();
+
+        // You can write bools and they use only one bit
+        writer.write_bool(true).unwrap();
+        
+        // You can write values with how many bits you wish
+        // Write the value 3 with only 2 bits
+        let value: u32 = 3;
+        writer.write_bits(value, 2).unwrap();
+
+        // You can also write write basic types
+        writer.write_u8(0).unwrap();  // uses 8 bits
+        writer.write_u16(1).unwrap();   // uses 16 bits
+        writer.write_u32(2).unwrap();   // uses 32 bits
+        writer.write_u64(3).unwrap();   // uses 64 bits
+        writer.write_i64(-1).unwrap();  // uses 16 bits
+        writer.write_i32(-2).unwrap();  // uses 32 bits
+        writer.write_i64(-3).unwrap();  // uses 64 bits
+
+        // But you can also use the varint encoding variation
+        // That tries to use the least number of bits to encode the value
+        writer.write_varint_u16(1).unwrap(); // uses 8 bits
+        writer.write_varint_u32(2).unwrap(); // uses 8 bits
+        writer.write_varint_u64(3).unwrap(); // uses 8 bits
+        writer.write_varint_i16(-1).unwrap();// uses 8 bits
+        writer.write_varint_i32(-2).unwrap();// uses 8 bits  
+        writer.write_varint_i64(-3).unwrap();// uses 8 bits
+        // Bigger values will use more than one byte
+
+        // Float types
+        writer.write_f32(1.0).unwrap(); // uses 32 bits
+        writer.write_f64(2.0).unwrap(); // uses 64 bits
+        
+        // Since the serializers impl Write/Read, we can use bincode
+        // Or write/read bytes directly to/from them
+        let bytes = vec![7u8; 20];
+        writer.write(&bytes).unwrap();
+
+        #[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+        struct SimpleStruct {
+            value: u64,
+            string: String,
+            array: [u16; 12],
+        }
+
+        let message = SimpleStruct {
+            value: 999999999999,
+            string: "some text to serialize".to_owned(),
+            array: [500; 12],
+        };
+
+        // Serialize using bincode passing a writer
+        bincode::serialize_into(&mut writer, &message).unwrap();
+
+        // Consume the writer to get the buffer, so we can create the reader
+        let writer_bytes = writer.consume().unwrap();
+        let mut reader = BitReader::new(&writer_bytes).unwrap();
+
+        // Now to the reading, just replace write for read, and do it in the same order :)
+        assert_eq!(reader.read_bool().unwrap(), true);
+        assert_eq!(reader.read_bits(2).unwrap(), 3);
+        
+        assert_eq!(reader.read_u8().unwrap(), 0);
+        assert_eq!(reader.read_u16().unwrap(), 1);
+        assert_eq!(reader.read_u32().unwrap(), 2);
+        assert_eq!(reader.read_u64().unwrap(), 3);
+        assert_eq!(reader.read_i64().unwrap(), -1);
+        assert_eq!(reader.read_i32().unwrap(), -2);
+        assert_eq!(reader.read_i64().unwrap(), -3);
+
+        assert_eq!(reader.read_varint_u16().unwrap(), 1);
+        assert_eq!(reader.read_varint_u32().unwrap(), 2);
+        assert_eq!(reader.read_varint_u64().unwrap(), 3);
+        assert_eq!(reader.read_varint_i16().unwrap(), -1);
+        assert_eq!(reader.read_varint_i32().unwrap(), -2);
+        assert_eq!(reader.read_varint_i64().unwrap(), -3);
+
+        assert_eq!(reader.read_f32().unwrap(), 1.0);
+        assert_eq!(reader.read_f64().unwrap(), 2.0);
+
+        let mut new_bytes = vec![0u8; bytes.len()];
+        reader.read(&mut new_bytes).unwrap();
+        assert_eq!(bytes, new_bytes);
+
+        let de_message: SimpleStruct = bincode::deserialize_from(&mut reader).unwrap();
+        assert_eq!(message, de_message);
+    }
 
     #[test]
     fn bit_writer_reader_test() {
